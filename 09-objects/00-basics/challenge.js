270a78f5a757e269ddb85742032940a0a98d8e67
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-07 19:48:58 +0000

    finished objects basics

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students) {
+  return {
+    title: title,
+    duration: duration,
+    students: students
+  };
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if ( object.hasOwnProperty(newProp) != true ) {
+    object[newProp] = newValue;
+  };
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter['recipient'] + ",\n\n" + letter['msg'] + "\n\nSincerely,\n" + letter['sender'];
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var prices = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+
+  return prices[item] <= money;
+};
