f7b216d3f424bbf48af762c19c978b299f8ea657
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-07 04:41:48 +0000

    completed array exercises

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,10 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, array) {
+  if (array.indexOf(item) == -1) { array.push(item) };
+  return array;
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(array) {
+  array.sort();
+  array.reverse();
+  return array;
+};
