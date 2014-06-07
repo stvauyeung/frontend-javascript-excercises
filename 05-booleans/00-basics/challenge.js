879bb9483f504633c8d9906c6bbb9a0c01e31e6f
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-07 03:32:49 +0000

    finished tests

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,23 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+  return stringOne === stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+  return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+  return lower < middle && upper > middle;
 };
 
 module.exports.outsideRanges = function(number) {
-
+  var returnValue = null;
+  if ((10 <= number && 20 >= number) || (42 < number && 75 >= number) || (1 < number && 6 > number)) { returnValue = false } else ( returnValue = true );
+  return returnValue;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+  var returnValue = false;
+  if ((name == 'NYTimes' || name == 'LATimes') && (price >= 1)) { returnValue = true };
+  return returnValue;
 };
